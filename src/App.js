d815c4ed1d7a427a7db48334384a2dd4c356c6d3
standera <standera@umich.edu>
2023-11-12 10:46:34 -0500

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import './App.css';
+import React, { Component } from 'react';
+import dotenv from 'dotenv';
+dotenv.config()
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      sensors: []
     }
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000/users')
+    fetch(`http://${process.env.HOST}:3000/sensors`)
       .then(response => response.json())
       .then(res => {
-        if (res.data) {
-          console.log(res.data)
+        if (res && res.data) {
+          this.setState({ sensors: [...this.state.sensors, ...res.data] })
         }
       })
+  }
+
+  renderSensors() {
+    if (this.state.sensors.length <= 0) {
+      return <div>Loading...</div>
+    }
+    else {
+      return this.state.sensors.map((val, key) => {
+        return <div key={key}>{val.sensor} | {val.data}</div>
+      });
+    }
   }
 
   render() {
     return (
       <div className="App">
-
+        {this.renderSensors()}
       </div>
     );
   }
 }
+
 export default App;
