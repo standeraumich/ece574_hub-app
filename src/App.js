1358d63aa95a1dd41a5c6b0be9bfe90364628f24
standera <standera@umich.edu>
2023-11-13 21:22:23 -0500

    Functional implementation

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
-import React, { Component } from 'react';
-import dotenv from 'dotenv';
-dotenv.config()
+import { useEffect, useState } from 'react';
+import Axios from 'axios'
 
-class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      sensors: []
-    }
+export default function App() {
+
+  const [sensorData, setSensorData] = useState({ sensors: [], DataisLoaded: false })
+
+  async function updateSensorLatest() {
+    await Axios.get(`http://${process.env.REACT_APP_API_HOST}:3001/sensors/latest`)
+      .then((res) => {
+        setSensorData({
+          sensors: res.data,
+          DataisLoaded: true
+        })
+      });
   }
 
-  componentDidMount() {
-    fetch(`http://${process.env.HOST}:3000/sensors`)
-      .then(response => response.json())
-      .then(res => {
-        if (res && res.data) {
-          this.setState({ sensors: [...this.state.sensors, ...res.data] })
-        }
-      })
-  }
-
-  renderSensors() {
-    if (this.state.sensors.length <= 0) {
+  function renderSensors() {
+    const { DataisLoaded, sensors } = sensorData;
+    if (!DataisLoaded) {
       return <div>Loading...</div>
     }
     else {
-      return this.state.sensors.map((val, key) => {
+      return sensors.map((val, key) => {
         return <div key={key}>{val.sensor} | {val.data}</div>
       });
     }
   }
 
-  render() {
-    return (
-      <div className="App">
-        {this.renderSensors()}
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSensorData({
+        sensors: sensorData.sensors,
+        DataisLoaded: false
+      })
+      updateSensorLatest();
+    }, 5000)
+    return () => clearInterval(intervalId);
+  }, [sensorData.sensors])
 
-export default App;
+  return (
+    <div className="App">
+      <div className='temperature-display-container'>
+        <div className='temperature-display'>{sensorData.sensors['thermistor']}</div>
+      </div>
+      <div className='button-container'>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  );
+
+}
